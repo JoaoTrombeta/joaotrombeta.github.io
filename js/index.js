63ba2c8c10d45fad199e796706d61b9638d9bcce
JoaoTrombeta <joaotrombetadev@gmail.com>
2025-02-12 15:49:33 -0300

    correcao foto

---
+++ js/index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const curriculo = document.getElementById('curriculo')
+    const foto = document.getElementById('myfoto')
+
+    curriculo.addEventListener('scroll', e => {
+        console.log(curriculo.scrollTop)
+        if(curriculo.scrollTop > 50){
+            foto.style.top = "15vh"
+            foto.style.left = "2.5vw"
+        }else if(curriculo.scrollTop < 100){
+            foto.style.top = "15vh"
+            foto.style.left = "45vw"
+        }
+    })
+})
